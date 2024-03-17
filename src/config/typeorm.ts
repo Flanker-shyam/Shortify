cd2e12464128e7af03d092f542e433802ecd47fb
Flanker-shyam <shyamp665@gmail.com>
2024-03-17 19:53:26 +0530

    remove config

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,16 +1,16 @@
 import { registerAs } from "@nestjs/config";
-import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from "typeorm";
+import * as dotenv from 'dotenv';
 
-dotenvConfig({ path: '.env' });
+dotenv.config();
 
 const config = {
-    type: 'postgres',
-    host: `localhost`,
-    port: `5432`,
-    username: `shyam.sunder`,
-    password: `secret`,
-    database: `url_shortner`,
+    type: process.env.DATABASE_TYPE||'postgres',
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME||`url_shortner`,
     entities: ["dist/**/*.entity{.ts,.js}"],
     migrations: ["dist/migrations/*{.ts,.js}"],
     autoLoadEntities: true,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NotFoundExceptionFilter } from './filters/not-found-exception.fllter';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new NotFoundExceptionFilter());
   await app.listen(3000);
