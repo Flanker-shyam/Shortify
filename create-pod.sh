613cee5cf8e8a0147f3b5dce09c55bc01b5f0704
Flanker-shyam <shyamp665@gmail.com>
2025-01-17 05:52:09 +0530

    fix migrations

--- create-pod.sh
+++ create-pod.sh
@@ -20,7 +20,7 @@ podman ps
 echo "-------------------------------------"
 
 echo "Creating a container for postgres with name postgres"
-podman run --pod shortify_pod -e POSTGRES_USER=flanker -e POSTGRES_PASSWORD=flanker -e POSTGRES_DB=url_shortner -d --name postgres postgres
+podman run --pod shortify_pod -e POSTGRES_USER=root -e POSTGRES_PASSWORD=root -e POSTGRES_DB=url_shortner -d --name postgres postgres
 postgres_container_id=$(podman ps | grep postgres | awk '{print $1}')
 echo "Container created successfully with container ID: $postgres_container_id"
 podman ps
@@ -28,7 +28,7 @@ echo "-------------------------------------"
 
 # Wait for PostgreSQL to be ready
 echo "Waiting for PostgreSQL to be ready"
-until podman exec $postgres_container_id pg_isready -U flanker; do
+until podman exec $postgres_container_id pg_isready -U root; do
     echo "PostgreSQL is not ready yet. Waiting..."
     sleep 2
 done
@@ -45,9 +45,26 @@ podman ps
 echo "-------------------------------------"
 
 echo "Container ID of shortify is $shortify_container_id"
-echo "Waiting for a few seconds to ensure the database is ready"
-sleep 10
 echo "Run DB migrations"
-podman exec -it $shortify_container_id npm run migration:run
-echo "DB migrations run successfully"
-echo "-------------------------------------"
+echo "keep executing job until migrations are applied"
+
+delay=5
+max_attempts=10
+COMMAND="podman exec -it $shortify_container_id npm run migration:run"
+
+for (( attempt=1; attempt <= $max_attempts; attempt++)); do
+    echo "Attempt: $attempt"
+    $COMMAND > log.txt
+    cat log.txt
+    if grep -q "Migration Migrations1710775141011 has been  executed successfully" log.txt; then
+        echo "Migrations applied successfully"
+        exit 0
+    else
+        echo "Migrations not applied yet. Waiting for $delay seconds"
+        sleep $delay
+        delay=$((delay + 2))
+    fi
+done
+
+echo "Max attempts reached. Migration failed."
+exit 1
